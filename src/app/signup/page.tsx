aed13af2f1400b7a66c6781244fc97d88288b4fb
Sandeep <sandeepchopra7@gmail.com>
2024-10-27 11:33:16 -0400

    add shadcn

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button';
+
+export default function Page() {
+  return (
+    <div>
+        <h1>Signup Page</h1>
+        <Button> Signup</Button>
+    </div>
+  )
+}
