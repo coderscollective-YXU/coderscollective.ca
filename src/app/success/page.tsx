d6328f520db4ab33c77cb1a71c8d0d2d64e4ea32
Sandeep <sandeepchopra7@gmail.com>
2024-10-28 14:33:21 -0400

    add favicion and update success page

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,7 +1,26 @@
-
+import { FlipWords } from "@/components/ui/flip-words";
+import React from "react";
+import MagicIcon from "../signup/magic-icon";
 
 export default function Success() {
+  const words = ["Learn", "Teach", "Support", "Connect"];
+
   return (
-    <>Successfully subscribed to Coders Collective Newsletter</>
+    <div className='min-h-screen bg-gray-900 justify-center items-center flex flex-col'>
+      <div className='text-xl text-neutral-50 flex mb-[5rem] items-center'>
+        <MagicIcon variant='dark' />
+        <div>
+          <div>Coders</div>
+          <div>Collective</div>
+        </div>
+      </div>
+      <div className='flex justify-center items-center px-4'>
+        <div className='text-2xl md:text-4xl mx-auto font-normal text-neutral-300 dark:text-neutral-400'>
+          Get ready to
+          <FlipWords words={words} /> <br />
+          Your subscription is confirmed.
+        </div>
+      </div>
+    </div>
   );
 }
